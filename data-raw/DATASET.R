d4d39ebec47a4d681f620278db85832f8ae2d0f1
autribasu <157545515+autribasu@users.noreply.github.com>
2024-03-12 22:09:21 -0400

    Wrangled data
    Removed redundant columns, edited one column to make it more readable, and added one more column that contained useful information

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,13 @@
 library(tidyverse)
+library(stringr)
 PoliticalViolence <-  
   readxl::read_xlsx("data-raw/asia_political_violence.xlsx") |>
-  janitor::clean_names() 
-usethis::use_data(PoliticalViolence)  
+  janitor::clean_names() |>
+  select(-admin3,-timestamp) |>
+  mutate(
+    civilian_targeting = ifelse(is.na(civilian_targeting), FALSE,TRUE),
+    crowd_size = ifelse(
+      str_detect(tags,"crowd size"),parse_number(tags),NA
+    )
+  ) 
+usethis::use_data(PoliticalViolence, overwrite = TRUE)
